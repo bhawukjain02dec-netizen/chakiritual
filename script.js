e5bb2cc875834da81cf74a540f7e1f32713ff86b
bhawukjain02dec-netizen <bhawukjain02dec@gmail.com>
2026-03-17 01:37:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting Chakiritual! We'll get back to you soon.");
+});
